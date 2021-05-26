6f036b5cbe2f8b8ed70b4a448523262bfe3acd88
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-26 16:46:09 +0530

    [Shreya] Add: Using Switch Case

--- EmpWageProblems/EmpWageProblems/EmployeWageProblem.cs
+++ EmpWageProblems/EmpWageProblems/EmployeWageProblem.cs
@@ -1,39 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EmpWageProblems
 {
-    class EmployeWageProblem
+    public class EmployeWageProblem
     {
-       // readonly int IS_PRESENT = 1;
-        readonly int Wage_Per_Hours = 20;
-        readonly int Emp_Full_Time = 2;
-        readonly int Emp_Part_Time = 1;
-         int empHrs;
-        int EmployeeWage;
-        public void Attendance()
+        const int Emp_Full_Time = 1;
+        const int Emp_Part_Time = 2;
+        public int Wage_Per_Hours = 20;
+        public int empHrs = 0;
+
+        public void Attendence()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-           // if (empCheck == IS_PRESENT)
-            //{
-              //  Console.WriteLine("Employee is Present");
-                if (empCheck == Emp_Part_Time)
-                {
-                    Console.WriteLine("Employee is Present");
-                    empHrs = 4;
-                }
-                else
-                {
-                    Console.WriteLine("Employee is Absent");
-                    empHrs = 0;
-                }
-            EmployeeWage = empHrs * Wage_Per_Hours;
-            Console.WriteLine("Employee Full Wage is : " + EmployeeWage);
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case Emp_Full_Time:
+                    empHrs = 8;
+                    break;
 
-         }     
-     }
+                case Emp_Part_Time:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            int EmployeeWage = empHrs * Wage_Per_Hours;
+            Console.WriteLine(EmployeeWage);
+        }
+    }
+
 }
