8300273e68b1baba7dae7527307e3c08541badc8
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-26 14:10:37 +0530

    [Shreya]Add: Welcome to Employee Wage Problem

---
+++ EmpWageProblems/EmpWageProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Program");
+            Console.ReadLine();
+        }
+
+    }
+}
