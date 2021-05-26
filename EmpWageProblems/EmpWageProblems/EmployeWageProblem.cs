685dc95f1d654cda8c14f92fd2878399497eb434
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-26 17:34:47 +0530

    [Shreya] Add: Calculating total working Hours or days is reached for a month

--- EmpWageProblems/EmpWageProblems/EmployeWageProblem.cs
+++ EmpWageProblems/EmpWageProblems/EmployeWageProblem.cs
@@ -11,34 +11,36 @@ namespace EmpWageProblems
         public int Wage_Per_Hours = 20;
         public int empHrs = 0;
         public int Working_Days_Per_Month = 20;
-        public int Total_Salary_Month = 0;
-
+        public int Total_Salary_Month = 100;
+        public int Total_Working_Hours = 100;
+        public int Working_Days = 0;
 
         public void Attendence()
         {
-            for (int day = 1; day <= Working_Days_Per_Month; day++)
+            while (empHrs < Total_Working_Hours && Working_Days < Working_Days_Per_Month)
             {
+                Working_Days++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-                    switch (empCheck)
-                    {
-                        case Emp_Full_Time:
-                            empHrs = 8;
+                switch (empCheck)
+                {
+                    case Emp_Full_Time:
+                        empHrs = 8;
                         break;
 
-                        case Emp_Part_Time:
-                            empHrs = 4;
+                    case Emp_Part_Time:
+                        empHrs = 4;
                         break;
 
-                        default:
-                            empHrs = 0;
-                        break;  
-                    }
-                int EmployeeWage = empHrs * Wage_Per_Hours;
-                Total_Salary_Month += EmployeeWage;
-                Console.WriteLine(EmployeeWage);
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                Total_Working_Hours += empHrs;
+               
             }
+            int EmpSalary = Total_Working_Hours + Wage_Per_Hours;
+            Console.WriteLine(EmpSalary);
         }
     }
-
 }
